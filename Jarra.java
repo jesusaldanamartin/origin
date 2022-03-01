06f5bf32e2e66a88934222a9a704b2dae1a7c297
Fran <francisco.rodriguezcordoba@uma.es>
2022-03-01 14:06:20 +0100

    Programador 1 ha hecho cambios

--- Jarra.java
+++ Jarra.java
@@ -15,18 +15,14 @@ package practicaGithub;
 		}
 		// metodos
 		// 1
+		public int capacidad(){
+ return this.capacidad;
+}
+public int cantidad(){
+ return this.cantidad;
+}
 		// 2
 		// 3
-		
-			public void llenaDesde(Jarra j){
-	while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
-	this.cantidad++;
-	j.cantidad--;
-	}
-	
-		public String toString(){
-	return ("("+this.capacidad+","+this.cantidad+")");
-	}
 		public void metodoComunitario(){
 		// Comentario incluido para pruebas
 		System.out.println("Aqui escribimos todos");
